00a6d708cba95e555f7ef5af4954e78186de286b
Shaun Prince <shaun@techfusion.ca>
2022-07-26 11:08:09 -0700

    Deploying sandbox VPC

---
+++ envs/sandbox.tfvars
@@ -0,0 +1,10 @@
+env = "sandbox"
+region = "us-east-1"
+vpc_name = "tada_sandbox"
+vpc_cidr = "10.22.0.0/16"
+private_subnets = ["10.22.32.0/21","10.22.40.0/21"]
+public_subnets = ["10.22.48.0/21","10.22.56.0/21"]
+enable_natgateway = true
+enable_vpngateway = false
+count_eip_nat = 1
+owner_tag = "Xerris DevOps Team"
---
+++ variables.tf
@@ -0,0 +1,59 @@
+variable "env" {
+    default =  "sandbox"
+}
+
+variable "create_vpc" {
+  default = true
+}
+
+variable "vpc_id" {
+  default = ""
+}
+
+variable "private_subnets_ids" {
+  default = []
+  type = list
+}
+
+variable "public_subnets_ids" {
+  default = []
+  type = list
+}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "vpc_name" {
+}
+
+variable "vpc_cidr" {
+}
+
+variable "private_subnets"{
+    type = list
+}
+
+variable "public_subnets"{
+    type = list
+}
+
+variable "enable_natgateway" {
+  default = true
+}
+
+variable "enable_vpngateway" {
+  default = false
+}
+
+variable "external_nat_ip_ids" {
+  default = [""]
+  type = list
+}
+variable "count_eip_nat" {
+  default = 1
+}
+
+variable "owner_tag" {
+    default = "Xerris DevOps Team"
+}
